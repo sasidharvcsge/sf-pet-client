14ef6092fa0d2ffe254d11a1500928ca47fc6ffe
sasidharvcsge <84582188+sasidharvcsge@users.noreply.github.com>
2021-05-29 12:54:50 -0400

    created initial project

---
+++ src/main/java/com/example/sfpetclient/SfPetClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfpetclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfPetClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfPetClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfpetclient/SfPetClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfpetclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
